dfb805fa28b2886969bb6852d24703e4be1373a5
Mini <minipillai@atmire.com>
2014-10-22 13:52:16 -0700

    w2p_22249_Implement Query Support in SPARQLChoiceAuthority

--- dspace/modules/additions/src/main/java/org/dspace/content/authority/SPARQLChoiceAuthority.java
+++ dspace/modules/additions/src/main/java/org/dspace/content/authority/SPARQLChoiceAuthority.java
@@ -7,17 +7,11 @@
  */
 package org.dspace.content.authority;
 
-import java.util.Iterator;
-import java.util.Arrays;
 import java.util.ArrayList;
-import java.util.List;
-
-import org.apache.commons.lang.ArrayUtils;
-import org.apache.log4j.Logger;
-
-import org.dspace.app.util.DCInputsReader;
-import org.dspace.app.util.DCInputsReaderException;
+ import com.hp.hpl.jena.query.Query;
+import org.dspace.core.ConfigurationManager;
 import org.dspace.core.SelfNamedPlugin;
+import com.hp.hpl.jena.query.*;
 
 /**
  * SPARQLChoiceAuthority source that reads the same input-forms which drive
@@ -43,50 +37,67 @@ import org.dspace.core.SelfNamedPlugin;
  */
 public class SPARQLChoiceAuthority extends SelfNamedPlugin implements ChoiceAuthority
 {
-    private static String values[] = {
-            "sun",
-            "mon",
-            "tue",
-            "wed",
-            "thu",
-            "fri",
-            "sat"
-    };
+    // contact URL from configuration
+    private static String endpoint_url = null;
+    private static String sparql_query = null;
 
-    private static String labels[] = {
-            "Sunday",
-            "Monday",
-            "Tuesday",
-            "Wednesday",
-            "Thursday",
-            "Friday",
-            "Saturday"
-    };
+
+    private String prefix="PREFIX db: <http://linked.rvdata.us/resource/>\n"+
+            "PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>\n" +
+            "PREFIX foaf: <http://xmlns.com/foaf/0.1/>\n" +
+            "PREFIX r2rmodel: <http://voc.rvdata.us/>\n" +
+            "PREFIX sf: <http://www.opengis.net/ont/sf#>\n" +
+            "PREFIX r2r: <http://linked.rvdata.us/vocab/resource/class/>\n" +
+            "PREFIX vcard: <http://www.w3.org/2001/vcard-rdf/3.0#>\n" +
+            "PREFIX dcterms: <http://purl.org/dc/terms/>\n" +
+            "PREFIX gn: <http://www.geonames.org/ontology#>\n" +
+            "PREFIX geosparql: <http://www.opengis.net/ont/geosparql#>\n" +
+            "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
+            "PREFIX d2r: <http://sites.wiwiss.fu-berlin.de/suhl/bizer/d2r-server/config.rdf#>\n" +
+            "PREFIX map: <http://linked.rvdata.us/resource/#>\n" +
+            "PREFIX owl: <http://www.w3.org/2002/07/owl#>\n" +
+            "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n" +
+            "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+            "PREFIX skos: <http://www.w3.org/2004/02/skos/core#>\n" ;
+
+
+    public SPARQLChoiceAuthority(){
+        if (endpoint_url == null)
+        {
+            endpoint_url = ConfigurationManager.getProperty("endpoint.url");
+
+            if (endpoint_url == null)
+            {
+                throw new IllegalStateException("Missing endpoint url");
+            }
+        }
+        /*if(sparql_query ==null)
+        {
+            sparql_query = prefix+ConfigurationManager.getProperty("sparql.query");
+
+            if(sparql_query == null)
+            {
+                 throw new IllegalStateException("Missing sparq query");
+            }
+        }  */
+    }
 
     public Choices getMatches(String field, String query, int collection, int start, int limit, String locale)
     {
         int dflt = -1;
         ArrayList<Choice> v = new ArrayList<Choice>();
-        //Choice v[] = new Choice[values.length];
-        for (int i = 0; i < values.length; ++i)
-        {
+        query = query.substring(0,1).toUpperCase()+query.substring(1,query.length());
 
+        sparql_query =prefix + "SELECT ?title WHERE { ?s a <http://linked.rvdata.us/vocab/resource/class/Cruise> . ?s dcterms:title ?title FILTER regex(?title, \'^"+query+"\') } ORDER BY ?s LIMIT 100";
 
-            if (values[i].startsWith(query))
-            {
-                dflt = i;
-                //v[j] = new Choice(String.valueOf(i), values[i], labels[i]);
-                v.add(new Choice(String.valueOf(i), values[i], labels[i]));
-            }
-        }
+         queryEndPoint(v);
 
-
+        // If query is wrong then fetch all titles
         if(v.size() == 0)
         {
-            for (int i = 0; i < values.length; ++i)
-            {
-                v.add(new Choice(String.valueOf(i), values[i], labels[i]));
-            }
+            sparql_query =prefix+"SELECT DISTINCT ?s ?p ?o WHERE { ?s a <http://linked.rvdata.us/vocab/resource/class/Cruise> . ?s <http://purl.org/dc/terms/title> ?o } ORDER BY ?s LIMIT 100";
+
+            queryAllTitle(v);
         }
         Choice[] choices = v.toArray(new Choice[v.size()]);
         return new Choices(choices, 0,v.size(), Choices.CF_AMBIGUOUS, false, dflt);
@@ -94,7 +105,7 @@ public class SPARQLChoiceAuthority extends SelfNamedPlugin implements ChoiceAuth
 
     public Choices getBestMatch(String field, String text, int collection, String locale)
     {
-        for (int i = 0; i < values.length; ++i)
+        /*for (int i = 0; i < values.length; ++i)
         {
             if (text.equalsIgnoreCase(values[i]))
             {
@@ -102,12 +113,39 @@ public class SPARQLChoiceAuthority extends SelfNamedPlugin implements ChoiceAuth
                 v[0] = new Choice(String.valueOf(i), values[i], labels[i]);
                 return new Choices(v, 0, v.length, Choices.CF_UNCERTAIN, false, 0);
             }
-        }
+        }  */
         return new Choices(Choices.CF_NOTFOUND);
     }
 
     public String getLabel(String field, String key, String locale)
     {
-        return labels[Integer.parseInt(key)];
+        //return labels[Integer.parseInt(key)];
+        return null;
+    }
+
+    public void queryEndPoint(ArrayList<Choice> choiceList)
+    {
+
+        Query query = QueryFactory.create(sparql_query);
+        QueryExecution qExe = QueryExecutionFactory.sparqlService(endpoint_url, query );
+        ResultSet results = qExe.execSelect();
+        while(results.hasNext())
+        {
+            QuerySolution row=results.nextSolution();
+            choiceList.add(new Choice(row.getLiteral("title").getString(), row.getLiteral("title").getString(), row.getLiteral("title").getString()));
+        }
+    }
+
+    public void queryAllTitle(ArrayList<Choice> choiceList)
+    {
+
+        Query query = QueryFactory.create(sparql_query);
+        QueryExecution qExe = QueryExecutionFactory.sparqlService(endpoint_url, query );
+        ResultSet results = qExe.execSelect();
+        while(results.hasNext())
+        {
+            QuerySolution row=results.nextSolution();
+            choiceList.add(new Choice(row.getLiteral("o").getString(), row.getLiteral("o").getString(), row.getLiteral("o").getString()));
+        }
     }
 }
